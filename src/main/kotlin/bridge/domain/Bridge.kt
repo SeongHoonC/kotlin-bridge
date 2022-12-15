4b8139922ae4c80b9140938cb4a4d3aa351c7b4b
vrexpert <vrexpert2302@gmail.com>
2022-12-15 15:27:49 +0900

    feat(Bridge): 다리 클래스 생성

---
+++ src/main/kotlin/bridge/domain/Bridge.kt
@@ -0,0 +1,11 @@
+package bridge.domain
+
+class Bridge(private val blocks:List<String>) {
+
+    fun isBroken(moving:String,index:Int):Boolean{
+        if(blocks[index] == moving){
+            return true
+        }
+        return false
+    }
+}
