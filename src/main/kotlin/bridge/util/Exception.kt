a5cadd552e4addf3c7bbb1da99c16ff1afb7c5cf
vrexpert <vrexpert2302@gmail.com>
2022-12-15 15:30:49 +0900

    feat(Exception): 예외처리 구현

--- src/main/kotlin/bridge/util/Exception.kt
+++ src/main/kotlin/bridge/util/Exception.kt
@@ -1,2 +1,24 @@
 package bridge.util
 
+import bridge.data.ErrorMessage.NOT_INT
+import bridge.data.ErrorMessage.SIZE_IN_RANGE
+import bridge.data.ErrorMessage.UP_OR_DOWN
+
+fun String.inputTypeException() {
+    try {
+        this.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException(NOT_INT)
+    }
+}
+
+fun Int.bridgeSizeException(){
+    require(this in 3..20){ SIZE_IN_RANGE}
+}
+fun String.upOrDownException() {
+    require(this == "U" || this == "D") { UP_OR_DOWN}
+}
+
+fun String.retryOrQuitException(){
+    require(this == "R" || this == "Q") { UP_OR_DOWN}
+}
